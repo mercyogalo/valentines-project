cffbfe6a098b167d8db941dce82136d49b85014b
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-04-26 16:31:51 -0700

    added javascript text to make a slideshow

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName(".moreInfo");
+  let slides = document.querySelectorAll(".moreInfo")
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 2000);
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
